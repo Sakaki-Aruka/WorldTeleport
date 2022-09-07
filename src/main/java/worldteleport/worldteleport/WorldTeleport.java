4aecb23a889b166946a9f4c09a68759bf494f737
Sakaki-Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2022-09-07 21:42:23 +0900

    upload : first commit

---
+++ src/main/java/worldteleport/worldteleport/WorldTeleport.java
@@ -0,0 +1,21 @@
+package worldteleport.worldteleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldTeleport extends JavaPlugin {
+
+    public void load(){
+        //
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
